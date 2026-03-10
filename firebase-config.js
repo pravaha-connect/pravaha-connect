18a3008564817566737488462d7ba4cdea30e298
Pravaha Connect <pravahaconnect@gmail.com>
2026-03-10 17:06:34 +0530

    data base setup

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,25 @@
 // Firebase Configuration
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { 
-    getAuth, 
-    signInWithEmailAndPassword, 
-    signInWithPopup, 
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    signInWithPopup,
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
     signOut,
-    onAuthStateChanged 
+    onAuthStateChanged
 } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBsuZL4U2H66C9tUKN91KXMm-l5umOa1vY",
-    authDomain: "flowlinkkiro.firebaseapp.com",
-    projectId: "flowlinkkiro",
-    storageBucket: "flowlinkkiro.firebasestorage.app",
-    messagingSenderId: "918596906932",
-    appId: "1:918596906932:web:55e2a235cefedd61098ce1",
-    measurementId: "G-B32FNF648G"
+    apiKey: "AIzaSyC-nMdM6vJIrAP2IpmGmeHab7F6eGs2yh0",
+    authDomain: "parvah-connect.firebaseapp.com",
+    projectId: "parvah-connect",
+    storageBucket: "parvah-connect.firebasestorage.app",
+    messagingSenderId: "512208446416",
+    appId: "1:512208446416:web:9fbf9984a79c1cf0514fd5",
+    measurementId: "G-DNE4S0J6SS"
 };
 
 // Initialize Firebase
